3151e8934fa99b021c05b685377a3629eb4c4631
Tamanna14-cpu <bimurto.bithy@gmail.com>
2021-11-11 00:34:43 +0600

    status update api created

--- index.js
+++ index.js
@@ -128,6 +128,19 @@ async function run() {
         //         });
         // });
 
+        // status update
+        app.put("/manageOrders/:id", async (req, res) => {
+            const filter = { _id: ObjectId(req.params.id) };
+            console.log(req.params.id);
+            const result = await ordersCollection.updateOne(filter, {
+                $set: {
+                    status: req.body.status,
+                },
+            });
+            res.send(result);
+            console.log(result);
+        });
+
     } finally {
         //   await client.close();
     }
