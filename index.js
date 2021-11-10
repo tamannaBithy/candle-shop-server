2799928d78d2e338408edf54dc4c334f4b7f8c3e
Tamanna14-cpu <bimurto.bithy@gmail.com>
2021-11-11 02:20:08 +0600

    manage products delete api is updated

--- index.js
+++ index.js
@@ -120,6 +120,15 @@ async function run() {
         })
 
 
+        // DELETE API
+        app.delete('/deleteMainProducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.json(result);
+        })
+
+
 
 
 
